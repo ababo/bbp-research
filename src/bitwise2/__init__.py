9b4dc07db447ea7f8ca47ebfaf6692bf3102d055
Simon Golbert <simongolbert@proton.me>
2025-05-22 16:34:32 +0000

    Add BitTensor.format()

--- src/bitwise2/__init__.py
+++ src/bitwise2/__init__.py
@@ -1,7 +1,7 @@
 """BitTensor and related generic Boolean tensor operations."""
 
 from enum import Enum
-from typing import TypeAlias, Union
+from typing import cast, TypeAlias, Union
 
 import torch
 
@@ -21,6 +21,17 @@ class BitTensor:
         self._bit_length = bit_length
         self._data = data
 
+    def __getitem__(self, index: int) -> "BitTensor":
+        if len(self._data.shape) < 2:
+            raise ValueError("bit indexing not supported")
+        return BitTensor(self._bit_length, self._data[index])
+
+    def __len__(self) -> int:
+        return self.shape[0]
+
+    def __str__(self) -> str:
+        return self.format()
+
     @property
     def shape(self) -> list[int]:
         """Return the shape of the tensor."""
@@ -30,6 +41,59 @@ class BitTensor:
     def data(self) -> torch.Tensor:
         """Return the underlying torch.Tensor."""
         return self._data
+
+    def format(
+        self,
+        margin: int = 0,
+        max_height: int = 24,
+        max_width: int = 80,
+    ) -> str:
+        """
+        Format the tensor as a string with optional indentation and line prefixes.
+
+        Args:
+            margin: String prepended to every line.
+            max_height: Maximum number of lines to display.
+            max_width: Maximum number of characters per line.
+
+        Returns:
+            A formatted string representation of the tensor.
+        """
+        if len(self.shape) == 1:
+            gen = (f"{(cast(int, v.item()) & 2**32-1):032b}"[::-1] for v in self._data)
+            bits = "".join(gen)[: self._bit_length]
+            if len(bits) > max_width:
+                ellipsis = "..."
+                eff_width = max_width - len(ellipsis)
+                bits = (
+                    bits[: eff_width // 2] + ellipsis + bits[-((eff_width + 1) // 2) :]
+                )
+            return bits
+
+        def fmt(item: "BitTensor") -> str:
+            return item.format(
+                margin=margin + 1,
+                max_height=(max_height - 1) // 2,
+                max_width=max_width - 2,
+            )
+
+        item0 = fmt(self[0])
+        item_height = len(item0.splitlines())
+
+        prefix = (margin + 1) * " "
+        result: str = "[" + item0
+
+        if item_height * len(self) > max_height:
+            for i in range(1, max_height // 2 // item_height):
+                result += "\n" + prefix + fmt(self[i])
+            result += "\n" + prefix + "..."
+            for i in range(len(self) - (max_height + 1) // 2 // item_height, len(self)):
+                result += "\n" + prefix + fmt(self[i])
+        else:
+            for i in range(1, len(self)):
+                result += "\n" + prefix + fmt(self[i])
+
+        return result + "]"
 
 
 BitLiteral: TypeAlias = Union[str, list["BitLiteral"]]
@@ -61,8 +125,8 @@ def bit_tensor(literal: BitLiteral, device: Device = Device.CPU) -> BitTensor:
 
     def parse_bits(bits: str) -> tuple[list[int], int]:
         bits = bits.replace("_", "")
-        if not all(c in "01" for c in bits):
-            raise ValueError("malformed bits (expected only 1, 0 or _)")
+        if bits == "" or not all(c in "01" for c in bits):
+            raise ValueError("malformed or empty bits (expected only 1, 0 or _)")
 
         padding = (32 - len(bits) % 32) % 32
         padded_bits = bits + "0" * padding
@@ -79,7 +143,7 @@ def bit_tensor(literal: BitLiteral, device: Device = Device.CPU) -> BitTensor:
         bit_length = -1
         for part in literal:
             int_part, length = parse_literal(part)
-            if bit_length != -1 and length != bit_length:
+            if bit_length not in (-1, length):
                 raise ValueError("inconsistent bit length")
             bit_length = length
             result.append(int_part)
--- tests/bitwise2/test_bitwise2.py
+++ tests/bitwise2/test_bitwise2.py
@@ -42,6 +42,41 @@ def test_bit_tensor():
     assert torch.equal(t.data, torch.tensor([-1], dtype=torch.int32))
 
 
+def test_bit_tensor_format():
+    """Test BitTensor.format()."""
+
+    t = bitwise2.bit_tensor("10110011100011110000111110000011111100000011111110000000")
+    assert (
+        t.format(max_width=56)
+        == "10110011100011110000111110000011111100000011111110000000"
+    )
+    assert (
+        t.format(max_width=55)
+        == "10110011100011110000111110...11111100000011111110000000"
+    )
+    assert (
+        t.format(max_width=54)
+        == "1011001110001111000011111...11111100000011111110000000"
+    )
+
+    t = bitwise2.bit_tensor(
+        [
+            [["0000"], ["0001"], ["0010"], ["0011"]],
+            [["0100"], ["0101"], ["0110"], ["0111"]],
+            [["1000"], ["1001"], ["1010"], ["1011"]],
+        ]
+    )
+    assert (
+        t.format(max_height=11)
+        == "[[[0000]\n  [0001]\n  [0010]\n  [0011]]\n ...\n [[1000]\n  [1001]\n  [1010]\n  [1011]]]"
+    )
+    assert (
+        t.format(max_height=12)
+        == "[[[0000]\n  [0001]\n  [0010]\n  [0011]]\n [[0100]\n  [0101]\n  [0110]\n  [0111]]\n "
+        + "[[1000]\n  [1001]\n  [1010]\n  [1011]]]"
+    )
+
+
 def test_bit_tensor_shape():
     """Test BitTensor.shape."""
 
