a4dd8b71ec096ac9c23514c92724ec6c68d2f0fe
Simon Golbert <simongolbert@proton.me>
2025-06-07 17:35:56 +0000

    Add test for bitwise_or_reduce()

---
+++ src/bitwise2/ext_cuda/bitwise_or_reduce.cu
@@ -0,0 +1,9 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <torch/extension.h>
+
+torch::Tensor bitwise_or_reduce(torch::Tensor input, int64_t dim)
+{
+    printf("bitwise_or_reduce (CUDA)\n");
+    return input;
+}
--- src/bitwise2/ext_cuda/module.cc
+++ src/bitwise2/ext_cuda/module.cc
@@ -1,15 +1,9 @@
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <pybind11/pybind11.h>
 #include <torch/extension.h>
 
 namespace py = pybind11;
 
-torch::Tensor bitwise_or_reduce(torch::Tensor input, int64_t dim, bool keepdim)
-{
-    printf("bitwise_or_reduce (CUDA)\n");
-    return input;
-}
+torch::Tensor bitwise_or_reduce(torch::Tensor input, int64_t dim);
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
@@ -17,8 +11,7 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
           "Perform bitwise OR reduction on an int32 PyTorch tensor.\n\n"
           "Args:\n"
           "    tensor (torch.Tensor): Input tensor of type int32.\n"
-          "    dim (int): Dimension along which to perform reduction.\n"
-          "    keepdim (bool, optional): Whether to keep the reduced dimension. Defaults to False.\n\n"
+          "    dim (int): Dimension along which to perform reduction.\n\n"
           "Returns:\n"
           "    torch.Tensor: Reduced tensor.",
           py::arg("tensor"), py::arg("dim"), py::arg("keepdim") = false);
